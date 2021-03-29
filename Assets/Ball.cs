409ebd02bbfae9523e90d71cf1259d55518d56d0
Majid <majidted@gmail.com>
2021-03-29 19:01:51 +0430

    feat: ball

---
+++ Assets/Ball.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+static class BallCoroutines {
+    public static string WaitForIt{ get { return "WaitForIt"; } }
+}
+
+public class BoundaryPosition {
+    public int lower{ get; set; }
+    public int upper{ get; set; }
+
+    public BoundaryPosition(int lower, int upper) {
+        this.lower = lower;
+        this.upper = upper;
+    }
+}
+
+public class Ball : MonoBehaviour
+{
+    public float secondsToWaitBeforeLaunching = 3;
+    public BoundaryPosition randomPositionX = new BoundaryPosition(6, 8);
+    public BoundaryPosition randomPositionY = new BoundaryPosition(-4, 3);
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rigidBody = GetComponent<Rigidbody>();
+        if(rigidBody) {
+            rigidBody.freezeRotation = true;
+        }
+        StartCoroutine(BallCoroutines.WaitForIt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator WaitForIt() {
+        var rigidBody = GetComponent<Rigidbody>();
+        yield return new WaitForSeconds(this.secondsToWaitBeforeLaunching);
+        if (rigidBody) {
+            rigidBody.AddForce(
+                Random.Range(this.randomPositionX.lower, this.randomPositionX.upper),
+                Random.Range(this.randomPositionY.lower, this.randomPositionY.upper),
+            0);
+        }
+    }
+}
